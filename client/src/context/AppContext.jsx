e3cf8a06f6116a8cdf542e20e1e7e9a88fc94df6
Ajay07102000 <18ajaysaini@gmail.com>
2025-01-03 18:01:48 +0530

    Login component frontend part completed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,17 +1,18 @@
 import { createContext, useState } from "react";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-const AppContextProvider = (props)=>{
-    const [user, setUser] = useState(null);
-    const value = {
-        user,setUser
-    }
-    return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-
-}
+const AppContextProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+  const value = {
+    user,
+    setUser,
+    showLogin,
+    setShowLogin,
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
 export default AppContextProvider;
