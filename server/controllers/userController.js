32b5b0fd6347e1cda785b747fedc49e59cbdf08d
Ajay07102000 <18ajaysaini@gmail.com>
2025-01-12 13:38:02 +0530

    changed bcrypt to bcryptjs for deployment

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel.js";
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
 
@@ -11,8 +11,8 @@ const registerUser = async(req,res)=>{
             return res.json({success:false , message : "Missing Details"})
         }
     
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password,salt)
 
     const userData= {
         name,
@@ -44,7 +44,7 @@ const loginUser = async(req,res)=>{
             return res.json({success:false , message : "User does not exist"})
         }
 
-        const isMatch = await bcrypt.compare(password,user.password);
+        const isMatch = await bcryptjs.compare(password,user.password);
 
         if(isMatch){
             const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
